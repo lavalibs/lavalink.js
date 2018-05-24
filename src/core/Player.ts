c189a47bf2bf72b6888367f3ae968b0c2ed10fd8
Will Nelson <nelson.will@live.com>
2018-05-24 11:38:56 -0700

    ignore track replacements on status ended

--- src/core/Player.ts
+++ src/core/Player.ts
@@ -21,7 +21,7 @@ export default class Player extends EventEmitter {
     this.guildID = guildID;
 
     this.on('event', (d) => {
-      if (d.type === 'TrackEndEvent') this.status = Status.ENDED;
+      if (d.type === 'TrackEndEvent' && d.reason !== 'REPLACED') this.status = Status.ENDED;
       else if (d.type === 'TrackExceptionEvent') this.status = Status.ERRORED
       else this.status = Status.STUCK;
     });
