1a9a2aa3587e083ff141bae24559c6ae1fc9976e
Yukine <blazylp@gmail.com>
2018-08-21 21:51:15 +0200

    fix correct uri encode of search queries for Node#load

--- src/core/Http.ts
+++ src/core/Http.ts
@@ -70,7 +70,7 @@ export default class Http {
   public load(identifier: string): Promise<TrackResponse[]> {
     const url = this.url();
     url.pathname = '/loadtracks';
-    url.search = `identifier=${identifier}`;
+    url.search = `identifier=${encodeURIComponent(identifier)}`;
 
     return this._make('GET', url);
   }
