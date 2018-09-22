a531c5dc7d1f814f549f5c9ce8ddbb58a1e231f6
Will Nelson <nelson.will@live.com>
2018-09-22 14:05:10 -0700

    update deps

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,10 @@ const gulp = require('gulp');
 const del = require('del');
 const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
+const merge = require('merge2');
 const project = ts.createProject('tsconfig.json');
 
-gulp.task('default', ['build']);
-
-gulp.task('build', () => {
+function build() {
   del.sync(['dist/**', '!dist']);
   del.sync(['typings/**', '!typings']);
 
@@ -14,6 +13,11 @@ gulp.task('build', () => {
     .pipe(sourcemaps.init())
     .pipe(project());
 
-  result.js.pipe(sourcemaps.write('.', { sourceRoot: '../src' })).pipe(gulp.dest('dist'));
-  result.dts.pipe(gulp.dest('typings'));
-});
+  return merge(
+    result.js.pipe(sourcemaps.write('.', { sourceRoot: '../src' })).pipe(gulp.dest('dist')),
+    result.dts.pipe(gulp.dest('typings')),
+  );
+}
+
+gulp.task('default', build);
+gulp.task('build', build);
