lavalibs/lavalink.js

commit 551792856fa110c499d87bc4a34e49d5835e2f09

delete voice info on disconnect

src/base/Node.ts
@@ -77,20 +77,27 @@ export default abstract class BaseNode extends EventEmitter {
     throw new Error('no available http module');
   }
 
-  public voiceStateUpdate(packet: VoiceStateUpdate) {
+  public voiceStateUpdate(packet: VoiceStateUpdate): Promise<boolean> {
     if (packet.user_id !== this.userID) return Promise.resolve(false);
 
-    this.voiceStates.set(packet.guild_id, packet.session_id);
-    return this._tryConnection(packet.guild_id);
+    if (packet.channel_id) {
+      this.voiceStates.set(packet.guild_id, packet.session_id);
+      return this._tryConnection(packet.guild_id);
+    } else {
+      this.voiceServers.delete(packet.guild_id);
+      this.voiceStates.delete(packet.guild_id);
+    }
+
+    return Promise.resolve(false);
   }
 
-  public voiceServerUpdate(packet: VoiceServerUpdate) {
+  public voiceServerUpdate(packet: VoiceServerUpdate): Promise<boolean> {
     this.voiceServers.set(packet.guild_id, packet);
     this._expectingConnection.add(packet.guild_id);
     return this._tryConnection(packet.guild_id);
   }
 
-  private async _tryConnection(guildID: string) {
+  private async _tryConnection(guildID: string): Promise<boolean> {
     const state = this.voiceStates.get(guildID);
     const server = this.voiceServers.get(guildID);
     if (!state || !server || !this._expectingConnection.has(guildID)) return false;

test/application.yml
@@ -4,9 +4,6 @@ server: # REST server
 lavalink:
   server:
     password: "youshallnotpass"
-    ws:
-      port: 8080
-      host: 0.0.0.0
     sources:
       youtube: true
       bandcamp: true

test/index.ts
@@ -11,7 +11,7 @@ const client = new Node({
   userID: process.env.USER_ID,
   hosts: {
     rest: 'http://localhost:8081',
-    ws: 'ws://localhost:8080',
+    ws: 'ws://localhost:8081',
   },
   send(guild, packet) {
     const conn = gateway.connections.get(0);
@@ -21,12 +21,15 @@ const client = new Node({
 });
 
 gateway.on('READY', console.log);
+client.on('event', console.log);
 
 gateway.on('MESSAGE_CREATE', async (shard, m) => {
   console.log(m.content);
-  if (m.content === 'join') await client.players.get('281630801660215296').join('281630801660215297');
-  if (m.content === 'leave') await client.players.get('281630801660215296').leave();
-  if (m.content === 'pause') await client.players.get('281630801660215296').pause();
+
+  const player = client.players.get('281630801660215296');
+  if (m.content === 'join') await player.join('281630801660215297');
+  if (m.content === 'leave') await player.leave();
+  if (m.content === 'pause') await player.pause();
 
   if (m.content === 'decode') {
     const trackResponse = await client.load('https://www.youtube.com/playlist?list=PLe8jmEHFkvsaDOOWcREvkgFoj6MD0pQ67');

commit 1fbcb738db2e267100639c2c68b666d312ab5ff5

fix(node): Cache the full VoiceStateUpdate; fixes Player.moveTo()
Currently, only the session_id is cached. When needed, a
VoiceStateUpdate is reconstructed by the getter Player.voiceState.
However, this "reconstructed" VSU is missing the channel_id property.
This break Player.moveTo(), because it calls Node.voiceStateUpdate()
with this partial VSU, which is basically a noop without the channel_id.

This commit fixes this issue by caching the full VoiceStateUpdate
(which includes the channel_id) in the Node.voiceStates Map.

This is the cleanest way I found. I cannot cache the channel_id in the
Player, because it may not exists yet when Node.voiceStateUpdate() is
called (and creating it at this point may be undesirable). Creating
another Map just to cache the channel_id looks wrong. Passing a fake
channel_id to Node.voiceStateUpdate() would works, but it's a ugly fix.

For the record, Player.moveTo() appears to have been broken by the
commit 551792856fa110c499d87bc4a34e49d5835e2f09 which changed the
behavior of Node.voiceStateUpdate() when the channel_id property is
missing.

src/base/Node.ts
@@ -44,7 +44,7 @@ export default abstract class BaseNode extends EventEmitter {
   public players: PlayerStore<this> = new PlayerStore(this);
   public http?: Http;
 
-  public voiceStates: Map<string, string> = new Map();
+  public voiceStates: Map<string, VoiceStateUpdate> = new Map();
   public voiceServers: Map<string, VoiceServerUpdate> = new Map();
 
   private _expectingConnection: Set<string> = new Set();
@@ -85,7 +85,7 @@ export default abstract class BaseNode extends EventEmitter {
     if (packet.user_id !== this.userID) return Promise.resolve(false);
 
     if (packet.channel_id) {
-      this.voiceStates.set(packet.guild_id, packet.session_id);
+      this.voiceStates.set(packet.guild_id, packet);
       return this._tryConnection(packet.guild_id);
     } else {
       this.voiceServers.delete(packet.guild_id);

src/core/Player.ts
@@ -138,14 +138,7 @@ export default class Player<T extends BaseNode = BaseNode> extends EventEmitter 
   }
 
   public get voiceState(): VoiceStateUpdate | undefined {
-    const session = this.node.voiceStates.get(this.guildID);
-    if (!session) return;
-
-    return {
-      guild_id: this.guildID,
-      user_id: this.node.userID,
-      session_id: session,
-    };
+    return this.node.voiceStates.get(this.guildID);
   }
 
   public get voiceServer(): VoiceServerUpdate | undefined {
@@ -228,10 +221,10 @@ export default class Player<T extends BaseNode = BaseNode> extends EventEmitter 
     this.node.players.delete(this.guildID);
   }
 
-  public voiceUpdate(sessionId: string, event: VoiceServerUpdate) {
+  public voiceUpdate(state: VoiceStateUpdate, server: VoiceServerUpdate) {
     return this.send('voiceUpdate', {
-      event,
-      sessionId,
+      event: server,
+      sessionId: state.session_id,
     });
   }
 